0cc6adea35337dfbe90173b8774f421f95203d58
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-12-07 04:00:55 +0300

    store results to postgres

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const stringify = require('csv-stringify');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const express = require('express');
 const app = express();
@@ -10,17 +11,57 @@ const jwktopem = require('jwk-to-pem');
 const jwt = require('jsonwebtoken');
 
 const config = JSON.parse(fs.readFileSync('config/config.json'));
+const secrets = JSON.parse(fs.readFileSync('secrets/secrets.json'));
+
+const sequelize = new Sequelize(secrets.db_url);
 
 // cloud native healthchecks
 const health = require('@cloudnative/health-connect');
 let healthcheck = new health.HealthChecker();
 
-const shutdownPromise = () => new Promise(function (resolve, _reject) {
-    setTimeout(function () {
-      console.log('DONE!');
-      resolve();
-    }, 10);
-  });
+const Result = sequelize.define('Result', {
+    // Model attributes are defined here
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    data: {
+      type: DataTypes.JSON,
+      allowNull: false
+      // allowNull defaults to true
+    },
+
+  }, {
+    // Other model options go here
+});
+
+const dbPromise = () => new Promise(async function (resolve, _reject) {
+    try {
+        await sequelize.authenticate();
+        console.log('DB connection alive.');
+        resolve();
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        _reject(error);
+    }
+});
+
+let readyCheck = new health.ReadinessCheck("dbCheck", dbPromise);//health.LivenessCheck("liveCheck", livePromise);
+healthcheck.registerReadinessCheck(readyCheck);
+healthcheck.registerLivenessCheck(readyCheck);
+
+
+const shutdownPromise = () => new Promise(async function (resolve, _reject) {
+    try {
+        await sequelize.close();
+        console.log('DB connection has been closed.');
+        resolve();
+    } catch (error) {
+        console.error('Unable to close the database:', error);
+        _reject(error);
+    }
+});
+
 let shutdownCheck = new health.ShutdownCheck("shutdownCheck", shutdownPromise);
 
 healthcheck.registerShutdownCheck(shutdownCheck);
@@ -28,21 +69,6 @@ app.use('/live', health.LivenessEndpoint(healthcheck));
 app.use('/ready', health.ReadinessEndpoint(healthcheck));
 
 app.use(express.json());
-
-
-// simple in memory storage
-// const example = {
-//     user_id: 'test_user',
-//     timestamp: 122233453345,
-//     data: {
-//         allow: [],
-//         deny: [],
-//         err: [],
-//         task_id: 'test_task'
-//     }
-// };
-
-const storage = [];
 
 async function table_to_csv(table) {
     return new Promise((resolve, reject) => {
@@ -58,19 +84,23 @@ async function table_to_csv(table) {
 
 async function csv() {
     const table = [['timestamp', 'user_id', 'task_id', 'status']];
-    storage.forEach(item => table.push([
-        item.timestamp, 
+    await Result.sync();
+    const results = await Result.findAll();
+    results.every(item => table.push([
+        item.createdAt, 
         item.user_id, 
         item.data.task_id, 
         item.data.deny.length == 0 ? 'OK' : 'FAIL'
-    ])); 
+    ]));
     return await table_to_csv(table);
 }
 
-function save(user_id, data) {
-    const timestamp = Date.now();
-    console.log(data.task_id)
-    storage.push({user_id, timestamp, data});
+async function save(user_id, data) {
+    console.log(data.task_id);
+    await Result.sync();
+    await Result.create({ user_id, data});
+
+    //storage.push({user_id, timestamp, data});
 }
 
 
@@ -109,28 +139,30 @@ async function authorize(req) {
 }
 
 app.post('/save', async function (req, res) {
-    const auth = await authorize(req);
-    if (auth.statusCode !== 200) {
-        res.status(auth.statusCode).end();
-        return;
+    try {
+        const auth = await authorize(req);
+        if (auth.statusCode !== 200) {
+            res.status(auth.statusCode).end();
+            return;
+        }
+        save(auth.decoded.sub, req.body);
+        res.end();
+    } catch (err) {
+        console.error(err);
+        res.status(503).end();
     }
-    save(auth.decoded.sub, req.body);
-    res.end();
 });
 
 
 app.get('/report', async function (req, res) {
-    res.set('Content-Type', 'text/plain');
-    res.send(await csv());
-    res.end();
+    try {
+        res.set('Content-Type', 'text/plain');
+        res.send(await csv());
+        res.end();
+    } catch (err) {
+        console.error(err);
+        res.status(503).end();
+    }
 });
-
-
-async function checkDB() {
-    throw new Error('not implemented');
-}
-
-// each check returns a Promise
-
 
 app.listen(3000);    
